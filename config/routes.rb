f891b2004348b5b63c9621f87f6614c77042c88d
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-15 12:32:45 -0200

    andando

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
   root to: 'restaurants#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
