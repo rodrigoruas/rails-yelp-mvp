b0d335450ce68bed9340f0951a48310284f41a55
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-15 11:41:37 -0200

    keep going

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Bibi",
+    address:      "Av Ataufo de Paiva",
+    phone_number: "2122364373",
+    category:     "belgian"
+  },
+  {
+    name:         "China in Box",
+    address:      "Rua Bulhoes de Carvalho",
+    phone_number: "2122239383",
+    category:     "chinese"
+  },
+  {
+    name:         "Spoleto",
+    address:      "Shopping da Gavea",
+    phone_number: "2122743992",
+    category:     "italian"
+  },
+  {
+    name:         "Japa B",
+    address:      "Rua J Seabra",
+    phone_number: "2122321383",
+    category:     "japanese"
+  },
+  {
+    name:         "Entrecote de Paris",
+    address:      "Rua Prudente de Moraes",
+    phone_number: "2122984343",
+    category:     "french"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
